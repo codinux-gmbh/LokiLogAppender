53a89582283abc24a862b74aa6bc969d40d5716f
dankito <git@dankito.net>
2023-04-19 22:10:07 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        kotlin("multiplatform") version kotlinVersion
+    }
+}
+
+
+rootProject.name = "LokiLogger"
+
