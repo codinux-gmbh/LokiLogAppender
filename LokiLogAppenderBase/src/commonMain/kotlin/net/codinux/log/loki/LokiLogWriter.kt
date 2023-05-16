fe8cdab99c9f90057abceda6ae79b483f57cd3d9
dankito <git@dankito.net>
2023-05-16 21:17:40 +0200

    Implemented escaping label names and determining prefixes only once

--- LokiLogAppenderBase/src/commonMain/kotlin/net/codinux/log/loki/LokiLogWriter.kt
+++ LokiLogAppenderBase/src/commonMain/kotlin/net/codinux/log/loki/LokiLogWriter.kt
@@ -18,13 +18,102 @@ open class LokiLogWriter(
     config: LogAppenderConfig,
     stateLogger: AppenderStateLogger = StdOutStateLogger(),
     private val webClient: WebClient = KtorWebClient(getLokiPushApiUrl(config.host), config.username, config.password, (config as? LokiLogAppenderConfig)?.tenantId)
-) : LogWriterBase<Stream>(config, stateLogger) {
+) : LogWriterBase<Stream>(escapeLabelNames(config), stateLogger) {
 
     companion object {
         private const val JsonContentType = "application/json"
 
         fun getLokiPushApiUrl(host: String): String =
             host + (if (host.endsWith('/')) "" else "/") + "loki/api/v1/push"
+
+        fun escapeLabelNames(config: LogAppenderConfig): LogAppenderConfig {
+            config.logLevelFieldName = escapeLabelName(config.logLevelFieldName)
+            config.loggerNameFieldName = escapeLabelName(config.loggerNameFieldName)
+            config.loggerClassNameFieldName = escapeLabelName(config.loggerClassNameFieldName)
+            config.threadNameFieldName = escapeLabelName(config.threadNameFieldName)
+
+            config.hostNameFieldName = escapeLabelName(config.hostNameFieldName)
+            config.hostIpFieldName = escapeLabelName(config.hostIpFieldName)
+            config.appNameFieldName = escapeLabelName(config.appNameFieldName)
+            config.stacktraceFieldName = escapeLabelName(config.stacktraceFieldName)
+
+            config.mdcKeysPrefix = determinePrefix(config.mdcKeysPrefix)
+
+            config.markerFieldName = escapeLabelName(config.markerFieldName)
+            config.ndcFieldName = escapeLabelName(config.ndcFieldName)
+
+            config.kubernetesFieldsPrefix = determinePrefix(config.kubernetesFieldsPrefix)
+            config.kubernetesLabelsPrefix = determinePrefix(config.kubernetesLabelsPrefix)
+            config.kubernetesAnnotationsPrefix = determinePrefix(config.kubernetesAnnotationsPrefix)
+
+            return config
+        }
+
+        /**
+         * It may contain ASCII letters and digits, as well as underscores and colons. It must match the regex [a-zA-Z_:][a-zA-Z0-9_:]*.
+         *
+         * Note: The colons are reserved for user defined recording rules. They should not be used by exporters or direct instrumentation.
+         *
+         * (And at least for Prometheus labels may not start with an underscore.)
+         *
+         * Label values may contain any Unicode characters.
+         *
+         * A label with an empty label value is considered equivalent to a label that does not exist.
+         *
+         * And:
+         *
+         * Labels are the index to Loki’s log data. They are used to find the compressed log content, which is stored separately as chunks.
+         * Every unique combination of label and values defines a stream, and logs for a stream are batched up, compressed, and stored as chunks.
+         *
+         * For Loki to be efficient and cost-effective, we have to use labels responsibly.
+         *
+         * (https://grafana.com/docs/loki/latest/fundamentals/labels/)
+         *
+         *
+         * Therefore:
+         * Do not use labels to store dimensions with high cardinality (many different label values), such as user IDs, email addresses, or other unbounded sets of values.
+         * (https://prometheus.io/docs/practices/naming/)
+         *
+         * or:
+         * Imagine now if you set a label for ip. Not only does every request from a user become a unique stream. Every request with a
+         * different action or status_code from the same user will get its own stream.
+         *
+         *
+         * Doing some quick math, if there are maybe four common actions (GET, PUT, POST, DELETE) and maybe four common status codes
+         * (although there could be more than four!), this would be 16 streams and 16 separate chunks. Now multiply this by every user
+         * if we use a label for ip. You can quickly have thousands or tens of thousands of streams.
+         *
+         * This is high cardinality. This can kill Loki.
+         *
+         * High cardinality causes Loki to build a huge index (read: $$$$) and to flush thousands of tiny chunks to the object store (read: slow).
+         * Loki currently performs very poorly in this configuration and will be the least cost-effective and least fun to run and use.
+         *
+         * (https://grafana.com/docs/loki/latest/fundamentals/labels/)
+         *
+         *
+         * As we see people using Loki who are accustomed to other index-heavy solutions (..)
+         * Large indexes are complicated and expensive. Often a full-text index of your log data is the same size or bigger than the log data itself.
+         * To query your log data, you need this index loaded, and for performance, it should probably be in memory. This is difficult to scale, and
+         * as you ingest more logs, your index gets larger quickly.
+         *
+         * Now let’s talk about Loki, where the index is typically an order of magnitude smaller than your ingested log volume. So if you are doing
+         * a good job of keeping your streams and stream churn to a minimum, the index grows very slowly compared to the ingested logs.
+         *
+         * When using Loki, you may need to forget what you know and look to see how the problem can be solved differently with parallelization.
+         * Loki’s superpower is breaking up queries into small pieces and dispatching them in parallel so that you can query huge amounts of log data in small amounts of time.
+         *
+         * (https://grafana.com/docs/loki/latest/fundamentals/labels/)
+         */
+        private fun escapeLabelName(fieldName: String): String {
+            // TODO: implement removing illegal characters by converting illegal characters to underscore. Do this once and set it in LoggerSettings
+            return fieldName.replace(' ', '_')
+        }
+
+        private fun determinePrefix(prefix: String?): String =
+            if (prefix.isNullOrBlank()) ""
+            else if (prefix.endsWith('.')) prefix.substring(0, prefix.length - 2) + "_" // in Loki prefixes get separated by '_', in ElasticSearch by '.'
+            else if (prefix.endsWith('_') == false) prefix + "_"
+            else prefix + "_"
     }
 
 
@@ -130,7 +219,7 @@ open class LokiLogWriter(
 
     protected open fun mapLabel(stream: Stream, includeField: Boolean, fieldName: String, value: String?) {
         if (includeField) {
-            stream.stream[escapeLabelName(fieldName)] = value
+            stream.stream[fieldName] = value
         }
     }
 
@@ -148,7 +237,7 @@ open class LokiLogWriter(
     private fun mapMdcLabels(stream: Stream, includeMdc: Boolean, mdc: Map<String, String>?) {
         if (includeMdc) {
             if (mdc != null) {
-                val prefix = determinePrefix(config.mdcKeysPrefix)
+                val prefix = config.mdcKeysPrefix
 
                 mdc.mapNotNull { (key, value) ->
                     addDynamicLabel(stream, prefix + key, value)
@@ -160,7 +249,7 @@ open class LokiLogWriter(
     private fun mapPodInfoLabels(stream: Stream) {
         if (config.includeKubernetesInfo) {
             podInfo?.let { info ->
-                val prefix = determinePrefix(config.kubernetesFieldsPrefix)
+                val prefix = config.kubernetesFieldsPrefix
 
                 mapLabel(stream, true, prefix + "namespace", info.namespace)
                 mapLabel(stream, true, prefix + "podName", info.podName)
@@ -179,78 +268,11 @@ open class LokiLogWriter(
         }
     }
 
-    /**
-     * It may contain ASCII letters and digits, as well as underscores and colons. It must match the regex [a-zA-Z_:][a-zA-Z0-9_:]*.
-     *
-     * Note: The colons are reserved for user defined recording rules. They should not be used by exporters or direct instrumentation.
-     *
-     * (And at least for Prometheus labels may not start with an underscore.)
-     *
-     * Label values may contain any Unicode characters.
-     *
-     * A label with an empty label value is considered equivalent to a label that does not exist.
-     */
-
-    // And:
-
-    /**
-     * Labels are the index to Loki’s log data. They are used to find the compressed log content, which is stored separately as chunks.
-     * Every unique combination of label and values defines a stream, and logs for a stream are batched up, compressed, and stored as chunks.
-     *
-     * For Loki to be efficient and cost-effective, we have to use labels responsibly.
-     *
-     * (https://grafana.com/docs/loki/latest/fundamentals/labels/)
-     */
-
-    // Therefore:
-    // Do not use labels to store dimensions with high cardinality (many different label values), such as user IDs, email addresses, or other unbounded sets of values.
-    // (https://prometheus.io/docs/practices/naming/)
-    //
-    // or:
-    // Imagine now if you set a label for ip. Not only does every request from a user become a unique stream. Every request with a
-    // different action or status_code from the same user will get its own stream.
-
-    /**
-     * Doing some quick math, if there are maybe four common actions (GET, PUT, POST, DELETE) and maybe four common status codes
-     * (although there could be more than four!), this would be 16 streams and 16 separate chunks. Now multiply this by every user
-     * if we use a label for ip. You can quickly have thousands or tens of thousands of streams.
-     *
-     * This is high cardinality. This can kill Loki.
-     *
-     * High cardinality causes Loki to build a huge index (read: $$$$) and to flush thousands of tiny chunks to the object store (read: slow).
-     * Loki currently performs very poorly in this configuration and will be the least cost-effective and least fun to run and use.
-     *
-     * (https://grafana.com/docs/loki/latest/fundamentals/labels/)
-     */
-
-    /**
-     *
-     * As we see people using Loki who are accustomed to other index-heavy solutions (..)
-     * Large indexes are complicated and expensive. Often a full-text index of your log data is the same size or bigger than the log data itself.
-     * To query your log data, you need this index loaded, and for performance, it should probably be in memory. This is difficult to scale, and
-     * as you ingest more logs, your index gets larger quickly.
-     *
-     * Now let’s talk about Loki, where the index is typically an order of magnitude smaller than your ingested log volume. So if you are doing
-     * a good job of keeping your streams and stream churn to a minimum, the index grows very slowly compared to the ingested logs.
-     *
-     * When using Loki, you may need to forget what you know and look to see how the problem can be solved differently with parallelization.
-     * Loki’s superpower is breaking up queries into small pieces and dispatching them in parallel so that you can query huge amounts of log data in small amounts of time.
-     *
-     * (https://grafana.com/docs/loki/latest/fundamentals/labels/)
-     */
-    private fun escapeLabelName(fieldName: String): String {
-        // TODO: implement removing illegal characters by converting illegal characters to underscore. Do this once and set it in LoggerSettings
-        return fieldName.replace(' ', '_')
-    }
-
     private fun escapeFieldValue(value: String): String =
         // we have to escape single backslashes as Loki doesn't accept control characters
         // (returns then 400 Bad Request invalid control character found: 10, error found in #10 byte of ...)
         value.replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t")
             .replace("\"", "\\\"")
-
-    private fun determinePrefix(prefix: String?): String =
-        if (prefix.isNullOrBlank()) "" else prefix + "_" // TODO: in Loki prefixes get separated by '_', in ElasticSearch by '.'
 
     // loggerName is in most cases full qualified class name including packages, try to extract only name of class
     protected open fun extractLoggerClassName(loggerName: String): String {
