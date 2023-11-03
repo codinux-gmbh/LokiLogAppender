7b697181d3e41968160d0a67905ac46b7e0d0368
dankito <git@dankito.net>
2023-11-03 22:56:38 +0100

    Added build step that enables reflection for model classes (in order to use kotlinx-serialization for them)

--- QuarkusLokiLoggerDeployment/src/main/java/net/codinux/log/loki/quarkus/QuarkusLokiLoggerSteps.java
+++ QuarkusLokiLoggerDeployment/src/main/java/net/codinux/log/loki/quarkus/QuarkusLokiLoggerSteps.java
@@ -4,7 +4,10 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.LogHandlerBuildItem;
-
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import net.codinux.log.loki.model.Stream;
+import net.codinux.log.loki.model.StreamBody;
+import net.codinux.log.loki.model.Values;
 import net.codinux.log.loki.quarkus.config.QuarkusLokiLogAppenderConfig;
 
 public class QuarkusLokiLoggerSteps {
@@ -13,6 +16,15 @@ public class QuarkusLokiLoggerSteps {
     @Record(ExecutionTime.RUNTIME_INIT)
     public LogHandlerBuildItem setUpLogAppender(QuarkusLokiLogAppenderRecorder recorder, QuarkusLokiLogAppenderConfig config) {
         return new LogHandlerBuildItem(recorder.initializeLokiLogAppender(config));
+    }
+
+    @BuildStep
+    ReflectiveClassBuildItem lokiLoggerClasses() {
+        return new ReflectiveClassBuildItem(true, true,
+                StreamBody.class, StreamBody.Companion.getClass(),
+                Stream.class, Stream.Companion.getClass(),
+                Values.class, Values.Companion.getClass(), Values.ValuesSerializer.class
+        );
     }
 
 }
