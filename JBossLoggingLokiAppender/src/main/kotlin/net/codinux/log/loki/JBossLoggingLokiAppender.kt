4bc4d11c233348e729b46b0fbdcef41380a9deca
dankito <git@dankito.net>
2023-11-12 22:54:02 +0100

    Adjusted to new signature

--- JBossLoggingLokiAppender/src/main/kotlin/net/codinux/log/loki/JBossLoggingLokiAppender.kt
+++ JBossLoggingLokiAppender/src/main/kotlin/net/codinux/log/loki/JBossLoggingLokiAppender.kt
@@ -6,4 +6,4 @@ import net.codinux.log.loki.config.LokiLogAppenderConfig
 import net.codinux.log.loki.config.LokiLogAppenderConfig.Companion.StateLoggerDefaultName
 
 open class JBossLoggingLokiAppender(config: LokiLogAppenderConfig)
-    : JBossLoggingAppenderBase(config.enabled, LokiLogWriter(config, JBossLoggingStateLogger(config.stateLoggerName ?: StateLoggerDefaultName)))
+    : JBossLoggingAppenderBase(LokiLogWriter(config, JBossLoggingStateLogger(config.stateLoggerName ?: StateLoggerDefaultName)))
