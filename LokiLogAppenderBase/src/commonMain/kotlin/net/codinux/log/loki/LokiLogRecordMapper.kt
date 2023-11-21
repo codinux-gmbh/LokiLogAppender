de50a6b7a8063308a424dfc0ee272e46d34d89b9
dankito <git@dankito.net>
2023-11-21 09:07:49 +0100

    Set new config value allowNullAsFieldValue to false

--- LokiLogAppenderBase/src/commonMain/kotlin/net/codinux/log/loki/LokiLogRecordMapper.kt
+++ LokiLogAppenderBase/src/commonMain/kotlin/net/codinux/log/loki/LokiLogRecordMapper.kt
@@ -4,7 +4,7 @@ import net.codinux.log.LogRecordMapper
 import net.codinux.log.config.LogAppenderFieldsConfig
 import net.codinux.log.loki.util.LokiLabelEscaper
 
-open class LokiLogRecordMapper(config: LogAppenderFieldsConfig) : LogRecordMapper(config, true) {
+open class LokiLogRecordMapper(config: LogAppenderFieldsConfig) : LogRecordMapper(config, false, true) {
 
     protected open val labelEscaper = LokiLabelEscaper()
 
