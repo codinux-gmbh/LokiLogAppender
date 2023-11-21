05b5a4900007426930960fbb70aa0eafc3896c5b
dankito <git@dankito.net>
2023-11-21 09:54:40 +0100

    Bumped version to 0.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ allprojects {
     }
 
     group = "net.codinux.log"
-    version = "0.5.3-SNAPSHOT"
+    version = "0.5.3"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux/LokiLogAppender"
