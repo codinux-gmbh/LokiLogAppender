e979188a522754f8f400ac0c38e08c5436bd3226
dankito <git@dankito.net>
2025-05-02 17:06:52 +0200

    Implemented setting the version of Gradle script in Maven pom.xml's

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,10 @@ allprojects {
 }
 
 
+gradle.taskGraph.whenReady {
+    setVersion(project.version.toString())
+}
+
 tasks.register("publishAllToMavenLocal") {
     dependsOn(
         ":LokiLogAppenderBase:publishToMavenLocal",
@@ -40,4 +44,25 @@ tasks.register("publishAll") {
         ":LogbackLokiAppender:publish",
         ":JBossLoggingLokiAppender:publish"
     )
+}
+
+
+fun setVersion(version: String) {
+    val projectDir = project.projectDir
+
+    val parentPomVersionRegex = Regex("^    <version>[\\dSNAPSHOT.-]+</version>", RegexOption.MULTILINE)
+    val childPomVersionRegex = Regex("^        <version>[\\dSNAPSHOT.-]+</version>", RegexOption.MULTILINE)
+
+    val parentPomFile = projectDir.resolve("pom.xml")
+    val parentPomText = parentPomFile.readText()
+    val parentPomTextUpdated = parentPomText.replaceFirst(parentPomVersionRegex, "    <version>$version</version>")
+    parentPomFile.writeText(parentPomTextUpdated)
+
+    listOf(projectDir.resolve("QuarkusLokiLogger"), projectDir.resolve("QuarkusLokiLoggerDeployment"))
+        .map { it.resolve("pom.xml") }
+        .forEach { childPomFile ->
+            val childPomText = childPomFile.readText()
+            val childPomTextUpdated = childPomText.replaceFirst(childPomVersionRegex, "        <version>$version</version>")
+            childPomFile.writeText(childPomTextUpdated)
+        }
 }
