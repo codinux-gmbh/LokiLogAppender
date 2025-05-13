2b5e58d941c1a9698673815869541f02ec85198f
dankito <git@dankito.net>
2025-05-13 20:17:07 +0200

    Made stateLogger and webClient configurable

--- JBossLoggingLokiAppender/src/main/kotlin/net/codinux/log/loki/JBossLoggingLokiAppender.kt
+++ JBossLoggingLokiAppender/src/main/kotlin/net/codinux/log/loki/JBossLoggingLokiAppender.kt
@@ -4,6 +4,17 @@ import net.codinux.log.JBossLoggingAppenderBase
 import net.codinux.log.statelogger.JBossLoggingStateLogger
 import net.codinux.log.loki.config.LokiLogAppenderConfig
 import net.codinux.log.loki.config.LokiLogAppenderConfig.Companion.StateLoggerDefaultName
+import net.codinux.log.loki.web.KtorWebClient
+import net.codinux.log.loki.web.WebClient
+import net.codinux.log.statelogger.AppenderStateLogger
 
-open class JBossLoggingLokiAppender(config: LokiLogAppenderConfig)
-    : JBossLoggingAppenderBase(LokiLogWriter(config, JBossLoggingStateLogger(config.stateLoggerName ?: StateLoggerDefaultName)))
+open class JBossLoggingLokiAppender(
+    config: LokiLogAppenderConfig,
+    stateLogger: AppenderStateLogger = JBossLoggingStateLogger(config.stateLoggerName ?: StateLoggerDefaultName),
+    webClient: WebClient = KtorWebClient.of(config, stateLogger),
+) : JBossLoggingAppenderBase(LokiLogWriter(config, stateLogger, webClient)) {
+
+    // Java does not support default parameters
+    constructor(config: LokiLogAppenderConfig) : this(config, JBossLoggingStateLogger(config.stateLoggerName ?: StateLoggerDefaultName))
+
+}
