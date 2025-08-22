0feb28ba523621f706606083abc82b2609be8534
dankito <git@dankito.net>
2025-08-22 15:36:38 +0200

    Bumped version to 0.8.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ allprojects {
     }
 
     group = "net.codinux.log"
-    version = "0.8.0"
+    version = "0.8.1-SNAPSHOT"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux-gmbh/LokiLogAppender"
